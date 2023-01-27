34c9fb7f229a0981807cbc516652aea0a1cd9783
Giorgia Marini <giorgiaj.marini@gmail.com>
2023-01-27 10:04:18 +0100

    Fixed id problem

--- process-test.php
+++ process-test.php
@@ -5,6 +5,8 @@ $gender = filter_input(INPUT_POST, "gender", FILTER_VALIDATE_INT);
 $age_range = filter_input(INPUT_POST, "age", FILTER_VALIDATE_INT);
 $perweek = filter_input(INPUT_POST, "perweek", FILTER_VALIDATE_INT);
 $genres = $_POST["genres"];
+$education = filter_input(INPUT_POST, "edu", FILTER_VALIDATE_INT);
+$profession = filter_input(INPUT_POST, "prof", FILTER_VALIDATE_INT);
 
 $userid = uniqid($more_entropy=TRUE); 
 $_SESSION['userid'] = $userid; 
@@ -15,7 +17,8 @@ include("questions.php");
 $serendipity = 0.46599 + 0.00764 * $o + 0.02122 * $c + (-0.01317) * $e + 0.00464 * $a + (-0.00402) * $n;
 $_SESSION['serendipity'] = $serendipity; 
 
-$sql1 = "INSERT INTO utente_ser (user_id, gender, age_range, perweek, genres, serendipity) VALUES (?, ?, ?, ?, ?, ?)";
+$sql1 = "INSERT INTO utente_ser (user_id, gender, age_range,education, profession, perweek, genres,  serendipity) 
+                VALUES (?, ?, ?, ?, ?, ?, ?, ?)";
 
 $stmt2 = mysqli_stmt_init($conn);
 
@@ -24,10 +27,12 @@ if (!mysqli_stmt_prepare($stmt2, $sql1)) {
     die(mysqli_error($conn));
 }
 
-mysqli_stmt_bind_param($stmt2, "iiiisi",
+mysqli_stmt_bind_param($stmt2, "siiiiisi",
                         $userid,
                        $gender,
                        $age_range,
+                       $education, 
+                       $profession,
                        $perweek,
                        $genres, 
                        $serendipity);
